925eadb55036feb179972eb501b36e821adc063e
Denis Seletkov <rysds2@gmail.com>
2016-10-02 21:57:47 +0400

    replaced the bubble, started fixing collisions

--- script.js
+++ script.js
@@ -2,13 +2,14 @@
 
 var WIDTH = 800;
 var HEIGHT = 600;
-var SIDE = 127; // px
-var INTERVAL = 15;
-var PADDING_TOP_L = 18;
-var PADDING_BTM_R = 20;
-var RAD = (SIDE - PADDING_BTM_R - PADDING_TOP_L) / 2;
+var SIDE = 128; // px
+var INTERVAL = 10;
+// var PADDING_TOP_L = 18;
+// var PADDING_BTM_R = 20;
+var PADD = 3; // px
+var RAD = SIDE / 2;
 var MIN_DV = 1;
-var MAX_DV = 5;
+var MAX_DV = 3;
 var SVG;
 var ID;
 var animating;
@@ -53,13 +54,13 @@ function Bubble(x, y, dx, dy, side) {
 	function move () {
 		p.x += v.x;
 		p.y += v.y;
-		if (p.x < -PADDING_TOP_L || p.x > WIDTH - SIDE + PADDING_BTM_R) {
+		if (p.x < -PADD || p.x > WIDTH - SIDE + PADD) {
 			v.x *= -1;
-			p.x = p.x < 0 ? -PADDING_TOP_L : WIDTH - SIDE + PADDING_BTM_R;
+			p.x = p.x < 0 ? -PADD : WIDTH - SIDE + PADD;
 		}
-		if (p.y < -PADDING_TOP_L || p.y > HEIGHT - SIDE + PADDING_BTM_R) {
+		if (p.y < -PADD || p.y > HEIGHT - SIDE + PADD) {
 			v.y *= -1;
-			p.y = p.y < 0 ? -PADDING_TOP_L : HEIGHT- SIDE + PADDING_BTM_R;
+			p.y = p.y < 0 ? -PADD : HEIGHT - SIDE + PADD;
 		}
 		bub.setAttribute('x', p.x);
 		bub.setAttribute('y', p.y);
@@ -80,19 +81,22 @@ function init(event) {
 	SVG.setAttribute('width', WIDTH);
 	SVG.setAttribute('height', HEIGHT);
 	SVG.addEventListener('click', function (event) {
-		if (!animating) {
-			animating = true;
-			startAnim();
+		if (event.clientX < WIDTH - RAD && event.clientX > RAD &&
+			event.clientY < HEIGHT - RAD && event.clientY > RAD) {
+			if (!animating) {
+				animating = true;
+				startAnim();
+			}
+			bubbles.push(new Bubble(
+				// rand_from_range(-PADDING_TOP_L, WIDTH - SIDE + PADDING_BTM_R),
+				// rand_from_range(-PADDING_TOP_L, HEIGHT - SIDE + PADDING_BTM_R),
+				event.clientX - RAD,
+				event.clientY - RAD,
+				rand_speed(),
+				rand_speed(),
+				SIDE
+			));
 		}
-		bubbles.push(new Bubble(
-			// rand_from_range(-PADDING_TOP_L, WIDTH - SIDE + PADDING_BTM_R),
-			// rand_from_range(-PADDING_TOP_L, HEIGHT - SIDE + PADDING_BTM_R),
-			rand_from_range(WIDTH - SIDE + PADDING_BTM_R),
-			rand_from_range(HEIGHT - SIDE + PADDING_BTM_R),
-			rand_speed(),
-			rand_speed(),
-			SIDE
-		));
 	});
 	SVG.addEventListener('contextmenu', function (event) {
 		event.preventDefault();
@@ -107,21 +111,39 @@ function startAnim() {
 		if (bubbles.length !== 1) {
 			for (var i = 0; i < bubbles.length - 1; i++) {
 			for (var j = i + 1; j < bubbles.length; j++) {
-				var x1 = bubbles[i].p.x + SIDE/2;
-				var y1 = bubbles[i].p.y + SIDE/2;
-				var x2 = bubbles[j].p.x + SIDE/2;
-				var y2 = bubbles[j].p.y + SIDE/2;
-				var v1 = bubbles[i].v;
-				var v2 = bubbles[j].v;
+				var bub1 = bubbles[i];
+				var bub2 = bubbles[j];
+				var x1 = bub1.p.x + SIDE/2 + PADD;
+				var y1 = bub1.p.y + SIDE/2 + PADD;
+				var x2 = bub2.p.x + SIDE/2 + PADD;
+				var y2 = bub2.p.y + SIDE/2 + PADD;
 				var dist = mod(x2 - x1, y2 - y1);
-				if (dist <= RAD*2) {
+				if (dist <= SIDE - PADD) {
 					console.log('collision');
-					var tx = v1.x;
-					var ty = v1.y;
-					v1.x = v2.x;
-					v1.y = v2.y;
-					v2.x = tx;
-					v2.y = ty;
+					var vx1 = bub1.v.x;
+					var vy1 = bub1.v.y;
+					var vx2 = bub2.v.x;
+					var vy2 = bub2.v.y;
+					var v1 = mod(vx1, vy1);
+					var v2 = mod(vx2, vy2);
+					var th1 = Math.asin(vx1 / v1); // first bubble angle
+					var th2 = Math.asin(vx2 / v2); // second bubble angle
+					var fi = Math.asin(Math.abs(bub1.p.x - bub2.p.x) / 
+						mod(bub1.p.x - bub2.p.x, bub1.p.y - bub2.p.y));
+					bub1.v.x = v2 * Math.cos(th2 - fi) * Math.cos(fi) +
+						v1 * Math.sin(th1 - fi) * Math.cos(fi + Math.PI/2);
+					bub1.v.y = v2 * Math.cos(th2 - fi) * Math.sin(fi) +
+						v1 * Math.sin(th1 - fi) * Math.sin(fi + Math.PI/2);
+					bub2.v.x = v1 * Math.cos(th1 - fi) * Math.cos(fi) +
+						v2 * Math.sin(th2 - fi) * Math.cos(fi + Math.PI/2);
+					bub2.v.y = v1 * Math.cos(th1 - fi) * Math.sin(fi) +
+						v2 * Math.sin(th2 - fi) * Math.sin(fi + Math.PI/2);
+					// var tx = v1.x;
+					// var ty = v1.y;
+					// v1.x = v2.x;
+					// v1.y = v2.y;
+					// v2.x = tx;
+					// v2.y = ty;
 				}
 			}
 			}
